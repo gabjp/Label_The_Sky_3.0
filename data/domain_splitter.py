0dc50227f22cca64f0690c3334daf0d4dafbc47f
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-09-29 18:34:03 -0300

    update

---
+++ data/domain_splitter.py
@@ -0,0 +1,172 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+DATA_PATH = "~/Label_The_Sky_2.0/Data/ready/clf"
+csv = pd.read_csv("./all/clf_90_5_5.csv")
+
+nowise = csv[csv['w1mpro'].isna()]
+dist1 = csv.dropna().sort_values("r_iso").iloc[0:27465]
+dist2 = csv.dropna().sort_values("r_iso").iloc[27465:54930]
+dist3 = csv.dropna().sort_values("r_iso").iloc[54930:82395]
+dist4 = csv.dropna().sort_values("r_iso").iloc[82395:]
+
+nowise_train = list(nowise[nowise.split == "train"]["inside_split_order"])
+dist1_train = list(dist1[dist1.split == "train"]["inside_split_order"])
+dist2_train = list(dist2[dist2.split == "train"]["inside_split_order"])
+dist3_train = list(dist3[dist3.split == "train"]["inside_split_order"])
+dist4_train = list(dist4[dist4.split == "train"]["inside_split_order"])
+
+nowise_val = list(nowise[nowise.split == "val"]["inside_split_order"])
+dist1_val = list(dist1[dist1.split == "val"]["inside_split_order"])
+dist2_val = list(dist2[dist2.split == "val"]["inside_split_order"])
+dist3_val = list(dist3[dist3.split == "val"]["inside_split_order"])
+dist4_val = list(dist4[dist4.split == "val"]["inside_split_order"])
+
+nowise_test = list(nowise[nowise.split == "test"]["inside_split_order"])
+dist1_test = list(dist1[dist1.split == "test"]["inside_split_order"])
+dist2_test = list(dist2[dist2.split == "test"]["inside_split_order"])
+dist3_test = list(dist3[dist3.split == "test"]["inside_split_order"])
+dist4_test = list(dist4[dist4.split == "test"]["inside_split_order"])
+
+class_train = np.load(DATA_PATH + "clf_90_5_5_class_train.npy")
+images_train = np.load(DATA_PATH + "clf_90_5_5_images_train.npy")
+tabular_train = np.load(DATA_PATH + "clf_90_5_5_tabular_train.npy")
+
+class_val = np.load(DATA_PATH + "clf_90_5_5_class_val.npy")
+images_val = np.load(DATA_PATH + "clf_90_5_5_images_val.npy")
+tabular_val = np.load(DATA_PATH + "clf_90_5_5_tabular_val.npy")
+
+class_test = np.load(DATA_PATH + "clf_90_5_5_class_test.npy")
+images_test = np.load(DATA_PATH + "clf_90_5_5_images_test.npy")
+tabular_test = np.load(DATA_PATH + "clf_90_5_5_tabular_test.npy")
+
+############################ NO_WISE SPLITS #########################
+
+nowise_tabular_train = tabular_train[nowise_train]
+nowise_class_train = class_train[nowise_train]
+nowise_images_train = images_train[nowise_train]
+
+nowise_tabular_val = tabular_val[nowise_val]
+nowise_class_val = class_val[nowise_val]
+nowise_images_val = images_val[nowise_val]
+
+nowise_tabular_test = tabular_test[nowise_test]
+nowise_class_test = class_test[nowise_test]
+nowise_images_test = images_test[nowise_test]
+
+np.save(nowise_tabular_train, "no_wise/tabular_train.npy")
+np.save(nowise_class_train, "no_wise/class_train.npy")
+np.save(nowise_images_train, "no_wise/images_train.npy")
+
+np.save(nowise_tabular_val, "no_wise/tabular_val.npy")
+np.save(nowise_class_val, "no_wise/class_val.npy")
+np.save(nowise_images_val, "no_wise/images_val.npy")
+
+np.save(nowise_tabular_test, "no_wise/tabular_test.npy")
+np.save(nowise_class_test, "no_wise/class_test.npy")
+np.save(nowise_images_test, "no_wise/images_test.npy")
+
+############################ DIST_1 SPLITS #########################
+
+dist1_tabular_train = tabular_train[dist1_train]
+dist1_class_train = class_train[dist1_train]
+dist1_images_train = images_train[dist1_train]
+
+dist1_tabular_val = tabular_val[dist1_val]
+dist1_class_val = class_val[dist1_val]
+dist1_images_val = images_val[dist1_val]
+
+dist1_tabular_test = tabular_test[dist1_test]
+dist1_class_test = class_test[dist1_test]
+dist1_images_test = images_test[dist1_test]
+
+np.save(dist1_tabular_train, "domain_1/tabular_train.npy")
+np.save(dist1_class_train, "domain_1/class_train.npy")
+np.save(dist1_images_train, "domain_1/images_train.npy")
+
+np.save(dist1_tabular_val, "domain_1/tabular_val.npy")
+np.save(dist1_class_val, "domain_1/class_val.npy")
+np.save(dist1_images_val, "domain_1/images_val.npy")
+
+np.save(dist1_tabular_test, "domain_1/tabular_test.npy")
+np.save(dist1_class_test, "domain_1/class_test.npy")
+np.save(dist1_images_test, "domain_1/images_test.npy")
+
+############################ DIST_2 SPLITS #########################
+
+dist2_tabular_train = tabular_train[dist2_train]
+dist2_class_train = class_train[dist2_train]
+dist2_images_train = images_train[dist2_train]
+
+dist2_tabular_val = tabular_val[dist2_val]
+dist2_class_val = class_val[dist2_val]
+dist2_images_val = images_val[dist2_val]
+
+dist2_tabular_test = tabular_test[dist2_test]
+dist2_class_test = class_test[dist2_test]
+dist2_images_test = images_test[dist2_test]
+
+np.save(dist2_tabular_train, "domain_2/tabular_train.npy")
+np.save(dist2_class_train, "domain_2/class_train.npy")
+np.save(dist2_images_train, "domain_2/images_train.npy")
+
+np.save(dist2_tabular_val, "domain_2/tabular_val.npy")
+np.save(dist2_class_val, "domain_2/class_val.npy")
+np.save(dist2_images_val, "domain_2/images_val.npy")
+
+np.save(dist2_tabular_test, "domain_2/tabular_test.npy")
+np.save(dist2_class_test, "domain_2/class_test.npy")
+np.save(dist2_images_test, "domain_2/images_test.npy")
+
+############################ DIST_3 SPLITS #########################
+
+dist3_tabular_train = tabular_train[dist3_train]
+dist3_class_train = class_train[dist3_train]
+dist3_images_train = images_train[dist3_train]
+
+dist3_tabular_val = tabular_val[dist3_val]
+dist3_class_val = class_val[dist3_val]
+dist3_images_val = images_val[dist3_val]
+
+dist3_tabular_test = tabular_test[dist3_test]
+dist3_class_test = class_test[dist3_test]
+dist3_images_test = images_test[dist3_test]
+
+np.save(dist3_tabular_train, "domain_3/tabular_train.npy")
+np.save(dist3_class_train, "domain_3/class_train.npy")
+np.save(dist3_images_train, "domain_3/images_train.npy")
+
+np.save(dist3_tabular_val, "domain_3/tabular_val.npy")
+np.save(dist3_class_val, "domain_3/class_val.npy")
+np.save(dist3_images_val, "domain_3/images_val.npy")
+
+np.save(dist3_tabular_test, "domain_3/tabular_test.npy")
+np.save(dist3_class_test, "domain_3/class_test.npy")
+np.save(dist3_images_test, "domain_3/images_test.npy")
+
+############################ DIST_4 SPLITS #########################
+
+dist4_tabular_train = tabular_train[dist4_train]
+dist4_class_train = class_train[dist4_train]
+dist4_images_train = images_train[dist4_train]
+
+dist4_tabular_val = tabular_val[dist4_val]
+dist4_class_val = class_val[dist4_val]
+dist4_images_val = images_val[dist4_val]
+
+dist4_tabular_test = tabular_test[dist4_test]
+dist4_class_test = class_test[dist4_test]
+dist4_images_test = images_test[dist4_test]
+
+np.save(dist4_tabular_train, "domain_4/tabular_train.npy")
+np.save(dist4_class_train, "domain_4/class_train.npy")
+np.save(dist4_images_train, "domain_4/images_train.npy")
+
+np.save(dist4_tabular_val, "domain_4/tabular_val.npy")
+np.save(dist4_class_val, "domain_4/class_val.npy")
+np.save(dist4_images_val, "domain_4/images_val.npy")
+
+np.save(dist4_tabular_test, "domain_4/tabular_test.npy")
+np.save(dist4_class_test, "domain_4/class_test.npy")
+np.save(dist4_images_test, "domain_4/images_test.npy")
