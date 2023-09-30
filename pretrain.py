7def0ff3d9571340c9aad364cc89f568c2ef5cb8
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-09-30 12:10:09 -0300

    update

---
+++ pretrain.py
@@ -0,0 +1,108 @@
+import argparse
+import torch
+from utils import get_loader, VGG16, test_l
+import torch.nn as nn
+import os
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description='Pretrain magnitude task')
+
+parser.add_argument('--save_dir', help='The directory used to save the trained models', default=None, type=str)
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--weight_decay', default=1e-4, type=float, help='weight decay')
+parser.add_argument('--epochs', default=100, type=int, help='number of total epochs to run')
+parser.add_argument('--dropout', default=0.3, type=float, help='dropout')
+parser.add_argument('--optimizer', default="adam", type=str, help="otimizer algorithm")
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+  
+class CustomMAE(nn.Module):
+    def __init__(self):
+        super(CustomMAE, self).__init__()
+        self.mae = mae = nn.L1Loss()
+
+    def forward(self, inputs, targets):
+        mvalue = 99/35
+        mask = torch.tensor(targets != mvalue, dtype=torch.float32)
+        return self.mae(inputs*mask, targets*mask)
+
+def main():
+    args = parser.parse_args()
+    print(args)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    #LOAD DATA
+    train_loader, val_loader, test_loader = get_loader("unl", args.batch_size)
+
+    #LOAD MODEL
+    model = VGG16(12, args.dropout)
+
+    #TRAIN
+    model = model.to(device)
+    criterion = CustomMAE()
+    if args.optimizer == "adam":
+        opt = torch.optim.Adam(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    else:
+        opt = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+    model.train()
+
+    train_loss = []
+    test_loss = []
+    best_loss = 1e10
+
+    for n_epoch in range(args.epochs):
+        epoch_train_loss = 0
+        num_batch_count = 0
+
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            opt.zero_grad()
+            out = model(images)
+
+            loss = criterion(out, labels)
+            loss.backward()
+            opt.step()
+
+            epoch_train_loss += loss.item()
+            num_batch_count +=1
+
+        val_loss = test_l(test_loader,model, criterion, device)
+        print(f"[{n_epoch+1}/{args.epochs}] - Training loss: {epoch_train_loss/num_batch_count} - Validation loss: {val_loss}")
+        train_loss.append(epoch_train_loss/num_batch_count)
+        test_loss.append(val_loss)
+        if val_loss < best_loss:
+            best_loss = val_loss
+            torch.save({
+                'epoch': n_epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': opt.state_dict(),
+                'loss': criterion,
+                }, args.save_dir + "/checkpoint.pth")
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(list(range(args.epochs)), test_loss, marker='o', linestyle='-', label='Validation Loss')
+    plt.title('Validation and Training Loss Curves')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.plot(list(range(args.epochs)), train_loss, marker='o', linestyle='-', label='Training Loss')
+    plt.grid(True)
+    plt.legend() 
+    plt.savefig(args.save_dir + '/loss_curves.png')
+    return 
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import torch 
 from torch import nn
+import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader, ConcatDataset
 # Implementação da VGG16 com batch normalization e dropout.
 # Fonte: https://blog.paperspace.com/vgg-from-scratch-pytorch/
 
@@ -75,3 +78,129 @@ class VGG16(nn.Module):
     out = self.fc1(out)
     out = self.fc2(out)
     return out
+  
+def test(testloader, model, criterion, device):
+  model.eval()
+  epoch_loss = 0
+  total_correct = 0
+  total_samples = 0
+  num_batch_count = 0
+
+  for i, (image, label) in enumerate(testloader):
+    image = image.to(device)
+    label = label.to(device)
+
+    out = model(image)
+    loss = criterion(out, label)
+
+    _, predicted = torch.max(out, 1)
+
+    epoch_loss += loss.item()
+    total_correct += (predicted == label).sum().item()
+    total_samples += label.size(0)
+    num_batch_count +=1
+
+  loss = epoch_loss / num_batch_count
+  acc = total_correct / total_samples * 100
+  model.train()
+  return loss, acc
+
+def test_l(testloader, model, criterion, device):
+  model.eval()
+  epoch_loss = 0
+  num_batch_count = 0
+
+  for i, (image, label) in enumerate(testloader):
+    image = image.to(device)
+    label = label.to(device)
+
+    out = model(image)
+    loss = criterion(out, label)
+
+    epoch_loss += loss.item()
+    num_batch_count +=1
+
+  loss = epoch_loss / num_batch_count
+  model.train()
+  return loss
+
+class FromNpyDataset(torch.utils.data.Dataset):
+  def __init__(self, X_path, y_path, is_unl = False):
+    super(FromNpyDataset, self).__init__()
+    # store the raw tensors
+    
+    self._x = np.load(X_path)
+    if is_unl:
+        self._y = np.load(y_path)[:,0:12]/35 
+    else:  
+        temp = np.load(y_path)
+        self._y = np.zeros((temp.size, temp.max() + 1))
+        self._y[np.arange(temp.size), temp] = 1
+
+  def __len__(self):
+    # a DataSet must know it size
+    return self._x.shape[0]
+
+  def __getitem__(self, index):
+    x = self._x[index, :]
+    y = self._y[index, :]
+    return x, y
+
+def get_loader(dataset, batch_size=64):
+
+    if dataset not in ["unl", "domain_1", "domain_2", "domain_3", "domain_4", "no_wise", "full"]:
+       return None  
+    
+    if dataset != "full":
+    
+        if dataset == "unl":
+            X_train_path = f"data/{dataset}/unl_w99_images_train.npy"
+            y_train_path = f"data/{dataset}/unl_w99_tabular_train.npy"
+
+            X_val_path = f"data/{dataset}/unl_w99_images_val.npy"
+            y_val_path = f"data/{dataset}/unl_w99_tabular_val.npy"
+
+            X_test_path = f"data/{dataset}/unl_w99_images_test.npy"
+            y_test_path = f"data/{dataset}/unl_w99_tabular_test.npy"
+        
+        else:
+            X_train_path = f"data/{dataset}/images_train.npy"
+            y_train_path = f"data/{dataset}/classes_train.npy"
+
+            X_val_path = f"data/{dataset}/images_val.npy"
+            y_val_path = f"data/{dataset}/classes_val.npy"
+
+            X_test_path = f"data/{dataset}/images_test.npy"
+            y_test_path = f"data/{dataset}/classes_test.npy"
+
+        train_set = FromNpyDataset(X_train_path, y_train_path, is_unl = dataset=="unl", train=True, transform=transforms.ToTensor())
+        val_set = FromNpyDataset(X_val_path, y_val_path, train=True, is_unl = dataset=="unl", transform=transforms.ToTensor())
+        test_set = FromNpyDataset(X_test_path, y_test_path, train=False, is_unl = dataset=="unl", transform=transforms.ToTensor())
+
+    else:
+        train_sets = []
+        val_sets = []
+        test_sets = []
+        for domain in ["domain_1", "domain_2", "domain_3", "domain_4"]:
+            X_train_path = f"data/{dataset}/images_train.npy"
+            y_train_path = f"data/{dataset}/classes_train.npy"
+
+            X_val_path = f"data/{dataset}/images_val.npy"
+            y_val_path = f"data/{dataset}/classes_val.npy"
+
+            X_test_path = f"data/{dataset}/images_test.npy"
+            y_test_path = f"data/{dataset}/classes_test.npy"
+
+            train_sets.append(FromNpyDataset(X_train_path, y_train_path, is_unl = dataset=="unl", train=True, transform=transforms.ToTensor()))
+            val_sets.append(FromNpyDataset(X_val_path, y_val_path, train=True, is_unl = dataset=="unl", transform=transforms.ToTensor()))
+            test_sets.append(FromNpyDataset(X_test_path, y_test_path, train=False, is_unl = dataset=="unl", transform=transforms.ToTensor()))
+        
+        train_set = ConcatDataset(train_sets)
+        val_set = ConcatDataset(val_sets)
+        test_set = ConcatDataset(test_sets)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=False, pin_memory=True)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+
+    return train_loader, val_loader, test_loader
