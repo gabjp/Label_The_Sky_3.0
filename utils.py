91c7cc091f29daf546917a54d051a16c98c3477f
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-12-30 16:24:33 -0300

    update

--- utils.py
+++ utils.py
@@ -183,9 +183,8 @@ class VGG16(nn.Module):
                       self.lin10, self.lin11, self.lin12]
   def forward(self, x):
     outs = []
-    print(x.size())
     for i in range(12): 
-      chn = x[:,:,:,i]
+      chn = x[:,i,:,:]
       chn = self.features[i](chn)
       chn = torch.reshape(chn, (-1,48))
       chn = self.lin[i](chn)
